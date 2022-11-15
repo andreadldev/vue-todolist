f398b58218803b806fcf206a9fa69712956ad01b
andreadldev <andrea.delisi2@gmail.com>
2022-11-15 15:47:59 +0100

    Script lista base

--- js/script.js
+++ js/script.js
@@ -3,10 +3,21 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-
+            newTodo: '',
+            todoList: [
+                'Fare i compiti',
+                'Fare la spesa',
+                'Andare dalla nonna'
+            ]
         }
     },
     methods: {
-
+        addTask() {
+            this.todoList.push(this.newTodo);
+            this.newTodo = '';
+        },
+        removeTask(index) {
+            this.todoList.splice(index, 1);
+        }
     }
 }).mount('#app');
